94b16d792d6180015ff695cd1bc24b3b4bde701d
MAKHMUDOV FARRUKH <farruhnet92@gmail.com>
2019-03-15 14:30:46 +0800

    no message

--- MakeIt/MakeIt/MakeIt/WelcomeController.swift
+++ MakeIt/MakeIt/MakeIt/WelcomeController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .cyan
         // Do any additional setup after loading the view, typically from a nib.
     }
 

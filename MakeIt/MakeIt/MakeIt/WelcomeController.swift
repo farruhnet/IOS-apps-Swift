4ab3c538405ddbc128eb6eac7f2d7c04a943016c
MAKHMUDOV FARRUKH <farruhnet92@gmail.com>
2019-03-15 14:50:37 +0800

    Displaying subView by implementing AutoLayout approach

--- MakeIt/MakeIt/MakeIt/WelcomeController.swift
+++ MakeIt/MakeIt/MakeIt/WelcomeController.swift
@@ -13,6 +13,7 @@ class WelcomeController: UIViewController {
     let bg:UIView = {
         let view = UIView()
         view.backgroundColor = .cyan
+        view.translatesAutoresizingMaskIntoConstraints = false
         view.layer.cornerRadius = 6
         return view
     }()
@@ -23,8 +24,9 @@ class WelcomeController: UIViewController {
         
         view.addSubview(bg)
         bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20).isActive = true
-        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
+        bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        bg.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
